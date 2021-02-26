5d06c9f034cc55e3bbe82ce3c596650c9e05ea1b
lujunjian <ljj>
2021-02-26 10:42:56 +0800

    新增uview-ui

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,7 @@ export default {
 </script>
 
 <style lang="scss">
+  @import "~uview-ui/index.scss";
   /* 全局样式 */
   @import "~@/styles/index";
 </style>
--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@
 
 import Vue from 'vue'
 import App from './App'
-
+import uView from 'uview-ui'
 import store from './store'
 import Api from '@/services/api.js'
 import Request from '@/services/request.js'
 import Utils from '@/utils/utils.js'
 import Validates from '@/utils/validates'
 
+Vue.use(uView)
 Vue.prototype.$api = Api
 Vue.prototype.$http = Request
 Vue.prototype.$utils = Utils
